0924b5e85b1d3dfe142f5b9d6bc5c3103287d85a
Michael Barrett <mjbarrett@mcw.edu>
2025-10-28 16:16:11 +0000

    clean slate

--- tests/__init__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2024-present barrettMCW <mjbarrett@mcw.edu>
-#
-# SPDX-License-Identifier: MIT
-"""example pytest module"""
--- tests/test_example.py
+++
@@ -1,5 +0,0 @@
-"""example pytest module"""
-from src.python_template.example import example
-def test_example():
-    """tests pytest"""
-    assert example(True) is True
